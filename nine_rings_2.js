7ceeaa806a991280b862fac96a1e5e47e32cb884
Source Academy User <No public email provided>
2024-09-26 00:47:40 +0800

    Changes made from Source Academy

--- nine_rings_2.js
+++ nine_rings_2.js
@@ -35,31 +35,29 @@ display(reverse(num_list2));
 
 
 function get_head(n, lst) {
-    return n === 1 ? null : pair(head(lst), get_head(n-1, tail(lst)));
-}
+    return n === 1 ? pair(head(lst)+1, null) : pair(head(lst), get_head(n-1, tail(lst)));
+}// get the first n-1 elements
 
 function get_tail(n, lst) {
     return n === 0 ? lst : get_tail(n-1, tail(lst));
+}// get the n+1 till the end element
+
+function step_choice(num_list, accessor) {
+    return is_null(num_list) ? null : list_ref(accessor, head(num_list) - 1) % 2 === 1 
+        ? pair("insert", step_choice(tail(num_list), 
+            append(get_head(head(num_list), accessor), 
+                    get_tail(head(num_list), accessor))))
+        : pair("remove", step_choice(tail(num_list), 
+            append(get_head(head(num_list), accessor), 
+                    get_tail(head(num_list), accessor))));
 }
 
+display(step_choice(num_list2, build_list(x => 0, length(rings))));
+const step_list2 = step_choice(num_list2, build_list(x => 0, length(rings)));
 
-
-// function step_choice(num_list, accessor) {
-//     return list_ref(accessor, head(num_list)-1) % 2 === 1 
-//     ? pair("insert", step_choice(tail(num_list), 
-//     append(list(get_head(head(num_list), accessor)), 
-//     pair(list_ref(head(num_list)-1) + 1, get_tail(head(num_list), accessor))))) 
-//     : pair("remove", step_choice(tail(num_list), 
-//     append(list(get_head(head(num_list), accessor)), 
-//     pair(list_ref(head(num_list)-1) + 1, get_tail(head(num_list), accessor)))));
-// }
-
-// step_choice(num_list2, build_list(x => 0, length(rings)));
-
-
-function steps(num_list) {
+function steps(num_list, step_list) {
     return is_null(num_list) ? null
-    : pair(list("step",head(num_list)), steps(tail(num_list)));
+    : pair(list(head(step_list),head(num_list)), steps(tail(num_list), tail(step_list)));
 }
 
-display_list(reverse(steps(num_list2)));
+display_list(reverse(steps(num_list2, step_list2)));

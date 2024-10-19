ab1fa67c84efa6b4d6bd483a6ecce1b73b897e4a
Source Academy User <No public email provided>
2024-10-19 19:05:06 +0800

    Changes made from Source Academy

---
+++ ra2.js
@@ -0,0 +1,22 @@
+// function f(x) {
+//     function g(y) {
+//         return (y===0) ? x : f(y-1);
+//     }
+//     return g(x);
+// }
+
+// f(10);
+
+// function curry(f) {
+//     return x => y => f(x, y);
+// }
+
+// curry(math_pow)(3)(4);
+
+// 0 === false;
+// 1 === 1 ? false && 0 : 1 && true;
+    // true ? false && 0 : 1 && true;
+        //   false && 0;
+//           false
+
+function binary
